915f54305d495cea69193ba030156aefb59c5ec2
Abdul Razak Shaikh <89abdul@gmail.com>
2024-04-28 14:39:29 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,8 +37,59 @@ document.addEventListener("DOMContentLoaded", function () {
 
     testEyeBtn.addEventListener("click", function () {
         const testEyeSection = document.getElementById("testeye-section");
-        testEyeSection.scrollIntoView({ behavior: "smooth" });
+        testEyeSection.scrollIntoView({ behavior: "smooth", block: "start" });
     });
 
 
 });
+
+
+
+// --------------------------COUNTER ON SCROLL--------------------------
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function countUp(element) {
+    var target = +element.getAttribute('data-target');
+    var count = +element.innerText;
+    var increment = target / 100; // Adjust the increment value as needed
+
+    if (count < target) {
+      element.innerText = Math.ceil(count + increment);
+      setTimeout(function() {
+        countUp(element);
+      }, 10); // Adjust the speed of counting here
+    } else {
+      element.innerText = target.toLocaleString();
+    }
+  }
+
+  function startCountersOnScroll() {
+    var counters = document.querySelectorAll('.counter');
+    var counterElements = Array.from(counters);
+
+    function updateCounters() {
+      counterElements.forEach(function(counter, index) {
+        if (isElementInViewport(counter)) {
+          countUp(counter);
+          counterElements.splice(index, 1);
+        }
+      });
+    }
+
+    // Initial check in case counters are already in view
+    updateCounters();
+
+    // Listen for scroll events
+    window.addEventListener('scroll', updateCounters);
+  }
+
+  // Start counting when the page is loaded
+  document.addEventListener('DOMContentLoaded', startCountersOnScroll);
